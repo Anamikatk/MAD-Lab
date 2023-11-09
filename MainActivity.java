d048b8c14f362904b617aa4eb4a16042382687f1
Anamika T K <116331823+Anamikatk@users.noreply.github.com>
2023-11-09 15:09:11 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,12 @@
-package com.example.myapplication;
-
+package com.example.ex6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,22 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void radioHandler(View view){
+        boolean selected=((RadioButton) view).isChecked();
+        System.out.println(selected);
+            if (view.getId() == R.id.r1) {
+                if (selected)
+                    Toast.makeText(this, "male selected", Toast.LENGTH_SHORT).show();
+            } else if (view.getId() == R.id.r2) {
+                if (selected)
+                    Toast.makeText(this, "female selected", Toast.LENGTH_SHORT).show();
+            }
 
-    public void checkSelect(View view) {
-        CheckBox checkBox= (CheckBox)view;
-        System.out.println(checkBox.isChecked());
-
-        if (view.getId() == R.id.c1) {
-            if (checkBox.isChecked())
-                Toast.makeText(MainActivity.this, "English is selected", Toast.LENGTH_SHORT).show();
-            else
-                Toast.makeText(MainActivity.this, "English is deselected", Toast.LENGTH_SHORT).show();
-        }
-        else if(view.getId() == R.id.c2) {
-            if(checkBox.isChecked())
-                Toast.makeText(MainActivity.this,"Malayalam is selected",Toast.LENGTH_SHORT).show();
-            else
-                Toast.makeText(MainActivity.this,"Malayalam is deselected",Toast.LENGTH_SHORT).show();
-        }
     }
 }
+
